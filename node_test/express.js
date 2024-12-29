5f3e6bb27b94036d81bb7c3d873c52165dc89669
kh9504 <k.hiraizumi0417@gmail.com>
2024-12-29 13:21:57 +0900

    expressテスト

--- node_test/express.js
+++ node_test/express.js
@@ -2,12 +2,15 @@ const port = 3000;
 const express = require("express");
 const app = express();
 
+app.use(express.static("static"));
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/view/index.html');
+  res.writeHead(200);
+  res.end();
 }); 
 
-app.get("/public/css/style.css", (req, res) => {
-  res.sendFile(__dirname + '/public/css/style.css');
-});
+// app.get("/public/css/style.css", (req, res) => {
+  // res.sendFile(__dirname + '/public/css/style.css');
+// });
 
 app.listen(port);
