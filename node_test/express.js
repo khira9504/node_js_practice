1ac49d09e96fc2dffa135a808f0946ea2cf0cbf3
kh9504 <k.hiraizumi0417@gmail.com>
2024-12-29 13:23:45 +0900

    expressテスト

--- node_test/express.js
+++ node_test/express.js
@@ -7,10 +7,6 @@ app.use(express.static("static"));
 app.get("/", (req, res) => {
   res.writeHead(200);
   res.end();
-}); 
-
-// app.get("/public/css/style.css", (req, res) => {
-  // res.sendFile(__dirname + '/public/css/style.css');
-// });
+});
 
 app.listen(port);
