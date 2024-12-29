b4966c8490c84c4855950cfef1f7cc2a31c69a11
kh9504 <k.hiraizumi0417@gmail.com>
2024-12-29 13:17:04 +0900

    expressテスト

--- node_test/express.js
+++ node_test/express.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello Express.js!</h1>");
-}).listen(port, () => {
-  console.log(`The server has started and is listening on port number: ${port}`);
+  res.sendFile(__dirname + '/view/index.html');
+}); 
+
+app.get("/public/css/style.css", (req, res) => {
+  res.sendFile(__dirname + '/public/css/style.css');
 });
+
+app.listen(port);
