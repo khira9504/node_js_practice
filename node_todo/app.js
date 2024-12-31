922d28bd45d5c95ec5968655df86327ba73ccf0a
kh9504 <k.hiraizumi0417@gmail.com>
2024-12-31 11:02:38 +0900

    cookie設定

--- node_todo/app.js
+++ node_todo/app.js
@@ -5,11 +5,16 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cookieSession = require("cookie-session");
 const secret = "secretCuisine123";
+
+app.use(cookieSession({
+  name: "session",
+  keys: [secret],
+  maxAge: 24 * 60 * 60 * 1000,
+}));
 
 app.use(logger("dev"));
 app.use(express.json());
